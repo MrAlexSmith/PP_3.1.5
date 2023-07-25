784cc928a9d3e2a8695dbf3e3273c858b68ea90b
MrAlexSmith <mr.alexander.smith@hotmail.com>
2023-07-26 01:56:58 +0300

    Views configuration is done.

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AppController.java
@@ -0,0 +1,62 @@
+package ru.kata.spring.boot_security.demo.controllers;
+
+import ru.kata.spring.boot_security.demo.models.User;
+import ru.kata.spring.boot_security.demo.services.UserService;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class AppController {
+
+    private final UserService userService;
+
+    public AppController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String showIndex() {
+        return "index1";
+    }
+
+    @GetMapping("/all-users")
+    public String showAllUsers(Model model) {
+        List<User> allUsers = userService.getAllUsers();
+        model.addAttribute("allUsers", allUsers);
+        return "all-users";
+    }
+
+    @GetMapping("/addNewUser")
+    public String addNewUser(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "user-info1";
+    }
+
+    @PostMapping("/saveUser")
+    public String saveUser(@ModelAttribute("user") User user) {
+        userService.saveUser(user);
+        return "redirect:/all-users";
+    }
+
+    @RequestMapping("/updateInfo")
+    public String updateUser(@RequestParam("usrId") int id, Model model) {
+        User user = userService.getUser(id);
+        model.addAttribute("user", user);
+        return "user-info1";
+    }
+
+    @RequestMapping("/deleteUser")
+    public String deleteUser(@RequestParam("usrId") int id) {
+        userService.deleteUser(id);
+        return "redirect:/all-users";
+    }
+}
